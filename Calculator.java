16fc1f1eb641cd45add668e89eced2958b067ec5
SungYub Jung <sungyup5003@gmail.com>
2017-08-08 14:54:50 +0900

    sum 함수 추가

--- Calculator.java
+++ Calculator.java
@@ -3,6 +3,12 @@ pakage com.mycompany.test;
 public class Calculator {
 
   public static void main(String[] args){
-//
+    int a = 4;
+    int b = 2;
+    System.out.println(sum(a,b));
+  }
+
+  private static int sum(int a,int b){
+    return a+b;
   }
 }
