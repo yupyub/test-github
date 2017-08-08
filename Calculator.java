f5d261719507caeeb9f796989f43c108dc87ab20
SungYub Jung <sungyup5003@gmail.com>
2017-08-08 14:57:00 +0900

    minus 함수 추가

--- Calculator.java
+++ Calculator.java
@@ -6,9 +6,14 @@ public class Calculator {
     int a = 4;
     int b = 2;
     System.out.println(sum(a,b));
+    System.out.println(minus(a,b));
   }
 
   private static int sum(int a,int b){
     return a+b;
+  }
+
+  private static int minus(int a,int b){
+    return a-b;
   }
 }
